6b90614893b267e73a027cc23059bacd70956c0c
arafat <arfatakbar090@gmail.com>
2023-12-01 22:06:23 +0530

    Initial commit

---
+++ src/day1/ConvertStringToLowerCase.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+//
+public class ConvertStringToLowerCase {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String inputString = scanner.nextLine();
+        String result = toLower(inputString);
+        System.out.println("String in lowercase: " + result);
+        scanner.close();
+    }
+    public static String toLower(String s) {
+        return s.toLowerCase();
+    }
+}
