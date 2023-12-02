80bfabba8f8e14031f177dc7982412f8bb8a0659
arafat <arfatakbar090@gmail.com>
2023-12-02 21:42:03 +0530

    Day 2 -> Reverse a String Done

---
+++ src/day2/ReverseString.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String inputString = scanner.nextLine();
+        String reversedString = reverseString(inputString);
+        System.out.println("Reversed String: " + reversedString);
+    }
+
+    static String reverseString(String input) {
+        StringBuilder reversed = new StringBuilder();
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed.append(input.charAt(i));
+        }
+        return reversed.toString();
+    }
+}
