5fbece257e2b09810a7e9cfd17cc1384ea77820b
arafat <arfatakbar090@gmail.com>
2023-12-12 18:11:35 +0530

    Day 11 -> Done

---
+++ src/day11/PowerOfTwo.java
@@ -0,0 +1,25 @@
+package day11;
+
+import java.util.Scanner;
+
+public class PowerOfTwo {
+
+    //??
+    public static boolean isPowerOfTwo(int n) {
+
+        if (n == 0)
+            return false;
+
+        while (n % 2 == 0) {
+            n /= 2;
+        }
+        return n == 1;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        System.out.println(isPowerOfTwo(n));
+    }
+}
