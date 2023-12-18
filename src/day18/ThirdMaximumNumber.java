7415cc8be5d3a12f868805753a5e05407504dd92
arafat <arfatakbar090@gmail.com>
2023-12-19 00:26:05 +0530

    Day 18 -> Done

---
+++ src/day18/ThirdMaximumNumber.java
@@ -0,0 +1,26 @@
+package day18;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class ThirdMaximumNumber{
+
+    //??
+    public static int thirdMax(int[] nums) {
+
+        Arrays.sort(nums);
+        HashSet<Integer> res = new HashSet<>();
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            res.add(nums[i]);
+            if (res.size() == 3) return nums[i];
+
+        }
+        return nums[nums.length - 1];
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(thirdMax(new int[]{1, 2, 3}));
+    }
+}
