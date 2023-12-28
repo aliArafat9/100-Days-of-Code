dfbdf7936dda2522d7d120f6d535f2885aa9f447
arafat <arfatakbar090@gmail.com>
2023-12-28 22:46:08 +0530

    Day 27 -> Done

---
+++ src/day27/IsSubsequence.java
@@ -0,0 +1,26 @@
+package day27;
+
+import java.util.Scanner;
+
+public class IsSubsequence {
+
+    //??
+    public static boolean isSubsequence(String s, String t) {
+        int i = 0;
+
+        for (int j = 0; j < t.length() && i < s.length(); j++) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+        }
+        return (i == s.length());
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        String t = scanner.nextLine();
+
+        System.out.println(isSubsequence(s, t));
+    }
+}
