b57368d54c319d92ecf20d59cbc5318cf529b210
arafat <arfatakbar090@gmail.com>
2024-01-06 14:45:06 +0530

    Day 35 -> Done

---
+++ src/day35/AssignCookies.java
@@ -0,0 +1,32 @@
+package day35;
+
+import java.util.Arrays;
+
+public class AssignCookies {
+
+    //??
+    public static int findContentChildren(int[] g, int[] s) {
+
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int i = 0; // cookies
+        int j = 0; // children
+        int count = 0;
+
+        while (i < g.length && j < s.length) {
+            if (s[j] < g[i]) {
+                j++;
+            } else {
+                i++;
+                j++;
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findContentChildren(new int[]{1, 2, 3}, new int[]{1, 1}));
+    }
+}
