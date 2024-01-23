5bca513ffa12583ae83beed36c21fdd15d2ae475
arafat <arfatakbar090@gmail.com>
2024-01-23 22:28:17 +0530

    Day 53 -> Done

---
+++ src/day53/TeemoAttacking.java
@@ -0,0 +1,20 @@
+package day53;
+
+
+public class TeemoAttacking{
+
+    //??
+    public static int findPoisonedDuration(int[] timeSeries, int duration) {
+
+        if (timeSeries.length == 0) return 0;
+        int totalTime = 0;
+        for (int i = 0; i < timeSeries.length - 1; i++) {
+            totalTime += Math.min(timeSeries[i + 1] - timeSeries[i], duration);
+        }
+        return totalTime + duration;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findPoisonedDuration(new int[]{1, 4}, 2));
+    }
+}
