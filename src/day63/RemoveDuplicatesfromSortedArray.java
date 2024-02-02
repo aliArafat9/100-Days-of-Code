98d32e582ec8ae4b33da98ecb812c258b84f667b
arafat <arfatakbar090@gmail.com>
2024-02-02 09:49:36 +0530

    Day 63 -> Done

---
+++ src/day63/RemoveDuplicatesfromSortedArray.java
@@ -0,0 +1,16 @@
+package day63;
+
+public class RemoveDuplicatesfromSortedArray {
+    public int removeDuplicates(int[] nums) {
+        int count = 0;
+        int reNum = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != reNum) {
+                reNum = nums[i];
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+        return count;
+    }
+}
