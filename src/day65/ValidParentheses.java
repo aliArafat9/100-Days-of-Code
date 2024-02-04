14f9b4379314f2045d8135d894eb4f8b47db0beb
arafat <arfatakbar090@gmail.com>
2024-02-04 14:22:55 +0530

    Day 65 -> Done

---
+++ src/day65/ValidParentheses.java
@@ -0,0 +1,30 @@
+package day65;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+    public static boolean isValid(String s) {
+        Stack<Character> st=new Stack<>();
+        if(s.charAt(0)==']'||s.charAt(0)==')'||s.charAt(0)=='}'){
+            return false;
+        }
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='(' || ch=='{' || ch=='['){
+                st.push(ch);
+            }
+            if(st.isEmpty() && (ch==']'||ch==')'||ch=='}')){
+                return false;
+            }
+            if((ch==')'&& st.peek()!='(')||(ch==']'&& st.peek()!='[')||(ch=='}'&& st.peek()!='{'))return false;
+            if(st.peek()=='(' && ch==')' ||st.peek()=='{' && ch=='}' ||st.peek()=='[' && ch==']' ){
+                st.pop();
+            }
+        }
+        return st.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isValid("abc"));
+    }
+}
