46617b79d859ce321cb1d84180c53b14eff3cb22
arafat <arfatakbar090@gmail.com>
2024-02-04 15:53:02 +0530

    Day 65 -> Done

--- src/day65/ValidParentheses.java
+++ src/day65/ValidParentheses.java
@@ -4,20 +4,19 @@ import java.util.Stack;
 
 public class ValidParentheses {
     public static boolean isValid(String s) {
-        Stack<Character> st=new Stack<>();
-        if(s.charAt(0)==']'||s.charAt(0)==')'||s.charAt(0)=='}'){
-            return false;
+        Stack<Character> st = new Stack<>();
+        if (s.isEmpty()) {
+            return true;  // An empty string is considered valid
         }
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            if(ch=='(' || ch=='{' || ch=='['){
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') {
                 st.push(ch);
-            }
-            if(st.isEmpty() && (ch==']'||ch==')'||ch=='}')){
+            } else if (st.isEmpty()) {
                 return false;
-            }
-            if((ch==')'&& st.peek()!='(')||(ch==']'&& st.peek()!='[')||(ch=='}'&& st.peek()!='{'))return false;
-            if(st.peek()=='(' && ch==')' ||st.peek()=='{' && ch=='}' ||st.peek()=='[' && ch==']' ){
+            } else if ((ch == ')' && st.peek() != '(') || (ch == ']' && st.peek() != '[') || (ch == '}' && st.peek() != '{')) {
+                return false;
+            } else if ((st.peek() == '(' && ch == ')') || (st.peek() == '{' && ch == '}') || (st.peek() == '[' && ch == ']')) {
                 st.pop();
             }
         }
