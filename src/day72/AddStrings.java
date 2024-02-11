75d7781c28df434002b908d7e7233ca55a4a3efd
arafat <arfatakbar090@gmail.com>
2024-02-11 18:21:29 +0530

    Day 72 -> Done

---
+++ src/day72/AddStrings.java
@@ -0,0 +1,25 @@
+package day72;
+
+public class AddStrings {
+    public static String addStrings(String num1, String num2) {
+        StringBuilder str = new StringBuilder();
+
+        int i = num1.length() - 1, j = num2.length() - 1, carry = 0, sum;
+
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int digit1 = (i >= 0) ? num1.charAt(i--) - '0' : 0;
+            int digit2 = (j >= 0) ? num2.charAt(j--) - '0' : 0;
+
+            sum = digit1 + digit2 + carry;
+            str.append(sum % 10);
+            carry = sum / 10;
+        }
+
+        return str.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        String result = addStrings("123", "456");
+        System.out.println("Sum: " + result);
+    }
+}
