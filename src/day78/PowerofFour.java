000bc68a073d0857eaa63cab6f47d1e8aba2efdf
arafat <arfatakbar090@gmail.com>
2024-02-17 15:58:39 +0530

    Day 78 -> Done

---
+++ src/day78/PowerofFour.java
@@ -0,0 +1,32 @@
+package day78;
+
+import java.util.Scanner;
+
+public class PowerofFour {
+    public static boolean isPowerOfThree(int n) {
+
+        if (n <= 0) {
+            return false;
+        }
+
+        while (n % 4 == 0) {
+            n = n / 4;
+        }
+
+        return n == 1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n = sc.nextInt();
+
+        boolean result = isPowerOfThree(n);
+
+        if (result) {
+            System.out.println(n + " is a power of four.");
+        } else {
+            System.out.println(n + " is not a power of four.");
+        }
+    }
+}
