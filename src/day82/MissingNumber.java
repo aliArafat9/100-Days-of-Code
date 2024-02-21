88b32df1596253fd749788e3275917bfcc5f1631
arafat <arfatakbar090@gmail.com>
2024-02-21 21:42:53 +0530

    Day 82 -> Done

---
+++ src/day82/MissingNumber.java
@@ -0,0 +1,32 @@
+package day82;
+
+import java.util.Arrays;
+
+public class MissingNumber {
+    public int missingNumber(int[] nums) {
+
+        int n = nums.length;
+        int[] v = new int[n + 1];
+        Arrays.fill(v, -1);
+
+        for (int i = 0; i < nums.length; i++) {
+            v[nums[i]] = nums[i];
+        }
+
+        for (int i = 0; i < v.length; i++) {
+            if (v[i] == -1)
+                return i;
+        }
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        MissingNumber missingNumberFinder = new MissingNumber();
+
+        int[] nums = {3, 0, 1};
+        int missingNumber = missingNumberFinder.missingNumber(nums);
+
+        System.out.println("The missing number is: " + missingNumber);
+    }
+}
